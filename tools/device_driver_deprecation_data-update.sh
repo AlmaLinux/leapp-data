efe3198348b2efc5c5bf7bc539c12f8adc10d536
Yuriy Kohut <yura.kohut@gmail.com>
2024-08-12 12:47:00 +0300

    Switch device driver deprecation data for almalinux, centos, eurolinux, oraclelinux and rocky into 'provided_data_streams' version '3.0' The script to update device driver deprication data: add an ability to check whether specific 'provided_data_streams' version is already set in the data

--- tools/device_driver_deprecation_data-update.sh
+++ tools/device_driver_deprecation_data-update.sh
@@ -10,7 +10,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 result_path="${parent_path}/../files/${dist_name}/"
 
 # Data stream version, which is currenyly supported by ELevate
-provided_data_streams="2.0"
+provided_data_streams="3.0"
 
 # Date stamp to add to device driver deprecation data
 date_stamp=$(date -u '+%Y%m%d%H%M%S')
@@ -25,13 +25,20 @@ device_driver_deprecation_data_json="device_driver_deprecation_data.json"
 leapp_repository_sha=9d0925f72c11adf95c4f80cd0633ac2aa1f133b1
 
 printf "\nDownload %s at %s\n" ${device_driver_deprecation_data_json} ${leapp_repository_sha}
-curl -o ${device_driver_deprecation_data_json} ${leapp_repository_url}/${leapp_repository_sha}/etc/leapp/files/${device_driver_deprecation_data_json} || exit 1
+curl -s -o ${device_driver_deprecation_data_json} ${leapp_repository_url}/${leapp_repository_sha}/etc/leapp/files/${device_driver_deprecation_data_json} || exit 1
 
 printf "\nAppend provided_data_streams with '%s'\n" ${provided_data_streams}
-jq --arg provided_data_streams "$provided_data_streams" '.provided_data_streams |= .+ [$provided_data_streams]' ${device_driver_deprecation_data_json} \
+cat << 'EOF' > tmp.jq
+if .provided_data_streams|index($provided_data_streams)|not
+then .provided_data_streams |= .+ [$provided_data_streams]
+else .
+end
+EOF
+jq --arg provided_data_streams "$provided_data_streams" -f tmp.jq ${device_driver_deprecation_data_json} \
     > ${device_driver_deprecation_data_json}.tmp
 [ -f ${device_driver_deprecation_data_json}.tmp ] && \
     mv -f ${device_driver_deprecation_data_json}.tmp ${device_driver_deprecation_data_json}
+rm -f tmp.jq
 
 printf "\nSet created_at to %s\n" "${date_stamp}"
 jq --arg created_at "$date_stamp" '.created_at |= $created_at' ${device_driver_deprecation_data_json} \
@@ -40,13 +47,13 @@ jq --arg created_at "$date_stamp" '.created_at |= $created_at' ${device_driver_d
     mv -f ${device_driver_deprecation_data_json}.tmp ${device_driver_deprecation_data_json}
 
 for version in "${releases[@]}"; do
-    printf "\nProcessing %s for AlmaLinux release %s\n" ${device_driver_deprecation_data_json} ${version}
+    printf "\nProcessing %s for AlmaLinux release %s ...\n" ${device_driver_deprecation_data_json} ${version}
 
     version_major=${version%.*}
     release_notes_md="${version}.md"
 
-    echo "Download ${release_notes_md} at ${release_notes_sha}"
-    curl -o ${release_notes_md} ${release_notes_url}/${release_notes_sha}/docs/release-notes/${release_notes_md} || exit 1
+    echo "  Download ${release_notes_md} at ${release_notes_sha}"
+    curl -s -o ${release_notes_md} ${release_notes_url}/${release_notes_sha}/docs/release-notes/${release_notes_md} || exit 1
 
     while read -r delimiter1 id delimiter2 name delimiter3 driver delimiter4; do
         echo "$id" | grep -E '0x.*:' >/dev/null || continue
@@ -58,13 +65,13 @@ for version in "${releases[@]}"; do
 
         if echo "'$id'" | grep -E '\*' >/dev/null; then
             id="^${id}"
-	    echo "Search rexexp '${id}'"
+	    echo "  Search rexexp '${id}'"
 
             cat << EOF > tmp.jq
 ( .data[] | select( .device_id? | match("$id"; "i") ) )
 EOF
         else
-	    echo "Search '${id}'"
+	    echo "  Search '${id}'"
             cat << EOF > tmp.jq
 ( .data[] | select( ( .device_id | ascii_downcase ) == "$id" ) )
 EOF
