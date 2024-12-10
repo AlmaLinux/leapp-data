7903c42e289038b984c3bf75494870ea7234e604
Yuriy Kohut <yura.kohut@gmail.com>
2024-12-10 17:41:57 +0200

    Drop EuroLinux support

--- tools/generate_map_pes_files.sh
+++ tools/generate_map_pes_files.sh
@@ -11,20 +11,17 @@ major_ver=$2
 declare -A os_repos
 os_repos["almalinux7"]="almalinux8-appstream almalinux8-powertools almalinux8-baseos"
 os_repos["centos7"]="centos8-appstream centos8-powertools centos8-baseos"
-os_repos["eurolinux7"]="certify-appstream certify-powertools certify-baseos"
 os_repos["oraclelinux7"]="ol8_appstream ol8_codeready_builder ol8-baseos"
 os_repos["rocky7"]="rocky8-appstream rocky8-powertools rocky8-baseos"
 
 os_repos["almalinux8"]="almalinux9-appstream almalinux9-crb almalinux9-baseos"
 os_repos["centos8"]="centos9-appstream centos9-crb centos9-baseos"
-os_repos["eurolinux8"]="certify-appstream certify-crb certify-baseos"
 os_repos["oraclelinux8"]="ol9_appstream ol9_codeready_builder ol9_baseos"
 os_repos["rocky8"]="rocky9-appstream rocky9-crb rocky9-baseos"
 
 declare -A os_name
 os_name["almalinux"]="AlmaLinux"
 os_name["centos"]="CentOS"
-os_name["eurolinux"]="EuroLinux"
 os_name["oraclelinux"]="OL"
 os_name["rocky"]="Rocky"
 
